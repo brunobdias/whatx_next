6a0d1abda6fb3f5c0fe52f9a0ca1ca604f4d7250
brunobdias <devbrunobdias@gmail.com>
2021-06-26 14:49:48 +0000

    Add Now Playng function and Carousel

--- app.py
+++ app.py
@@ -16,17 +16,65 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 app.api_key = os.environ.get("API_KEY")
+app.api_version = os.environ.get("API_VERSION")
 
 mongo = PyMongo(app)
 
-@app.route("/", methods=['GET'])
-@app.route("/get_movies", methods=['GET'])
-def get_movies():
+api_url_src = "https://api.themoviedb.org/"
+endpoint_path = None
+
+### URL SAMPLE 
+# https://api.themoviedb.org/3/movie/now_playing?api_key=cb840be2847e004061e5c0d2c9f0f0aa&language=en-US
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/now_playing", methods=['GET', 'POST'])
+def now_playing():
+    endpoint_path = "/movie/now_playing"
+    endpoint_api_key = f"?api_key={app.api_key}"
+    endpoint_lang = "&language=en-US"
+    img_url_endpoint_size = "https://image.tmdb.org/t/p/w500"
+    endpoint = f"{api_url_src}{app.api_version}{endpoint_path}{endpoint_api_key}{endpoint_lang}"
+    req = requests.get(endpoint)
+    pprint.pprint(endpoint)
+    if req.status_code == 200:
+        data = req.json()
+        results = data['results']
+        if len(results) > 0:
+            print(results[0].keys())
+            movie_ids = set()
+            for result in results:
+                img_url = f"{img_url_endpoint_size}{result['poster_path']}"
+                result['poster_path'] = img_url
+                _id = result['id']
+                title = result['title']
+                print(title, _id)
+                movie_ids.add(_id)
+            movies = results
+    
+    return render_template("get_movies.html", movies=movies)
+
+    """
+GET
+/movie/latest
+Get the most newly created movie. This is a live response and will continuously change.
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text":{"$search": query}}))
+    return render_template("get_movies.html", tasks=tasks)
+
+
+    GET
+/search/movie
+
+https://api.themoviedb.org/3/search/movie?api_key=cb840be2847e004061e5c0d2c9f0f0aa&language=en-US&query=army&page=1&include_adult=false
+
     req = requests.get("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
     print(json.loads(req.content))
     movies = json.loads(req.content)
     return render_template("get_movies.html", movies=movies)
-
+    """
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.carousel').carousel({
+        dist: -50,
+        numVisible: 15
+    });
   });
