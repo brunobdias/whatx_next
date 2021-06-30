ddef844295c4e713a4cd9c3632709e8e41fa5b1a
brunobdias <devbrunobdias@gmail.com>
2021-06-30 20:18:43 +0000

    Create login page, and attempt to login using google acc

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
+import json
 from flask import (
     Flask, flash, render_template, 
     redirect, request, session, url_for)
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import json
-import pprint
+
 if os.path.exists("env.py"):
     import env
 import requests
+import pprint
 
 app = Flask(__name__)
 
@@ -18,10 +27,23 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.api_key = os.environ.get("API_KEY")
 app.api_version = os.environ.get("API_VERSION")
 
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+
 mongo = PyMongo(app)
 
 api_url_src = "https://api.themoviedb.org/"
 endpoint_path = None
+
+# GOOGLE AUTH SETUP
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 """
     list_type: now_playing, popular, top_rated, upcoming
@@ -80,6 +102,7 @@ def view_movie(movie_id):
         movies = data
     return render_template("view_movies.html", movies=movies)
 
+
 ### Get list_type return list_name
 def get_list_name(list_type):
     char_remove = "_"
@@ -87,6 +110,84 @@ def get_list_name(list_type):
         list_name = list_type.replace(char_remove[i]," ").title()
     return list_name
 
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login_google")
+def login_google():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    return redirect(url_for("list_movies"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("list_movies"))
 """
 GET
 /movie/latest
