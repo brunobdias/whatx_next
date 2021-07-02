bf4028fa80ac685dfff0c9017308add22b140c70
brunobdias <devbrunobdias@gmail.com>
2021-07-02 14:46:20 +0000

    Fix Carousel Center item, add div and styling resing and controlling when on smaller screens

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
+    if ($(window).width() < 470) {
+        $('.carousel').carousel({
+        });
+    } else {  
+        $('.carousel').carousel({
+            dist: -50,
+            numVisible: 20,
+            indicators: false,
+            padding: 100
+        });
+    }
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
-    $('.carousel').carousel({
-        dist: -50,
-        numVisible: 20,
-        indicators: false,
-        padding: 100
-    });
     $('.collapsible').collapsible();
   });
