1e184f3483323a462fe29d9790db235e2302a6e7
brunobdias <devbrunobdias@gmail.com>
2021-07-02 10:55:26 +0000

    Add chip with username logged

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
     $('.carousel').carousel({
         dist: -50,
         numVisible: 20,
         indicators: false,
         padding: 100
     });
-    $('.tooltipped').tooltip();
     $('.collapsible').collapsible();
   });
