0405ab78870456f808f09abdb810d5cdb087b8ce
brunobdias <devbrunobdias@gmail.com>
2021-07-13 19:14:49 +0000

    Add Contact Section Sending email

---
+++ static/js/sendEmail.js
@@ -0,0 +1,25 @@
+function sendEmail(contactForm) {
+
+    let templateParams = {
+      from_name: contactForm.name.value,
+      from_email: contactForm.email.value,
+      message: contactForm.comment.value
+    };
+  
+    emailjs
+      .send("service_qrmt_whatxNext", "template_vf90o8b", templateParams)
+      .then(
+        function (response) {
+          //Credit to https://stackoverflow.com/questions/6653556/jquery-javascript-function-to-clear-all-the-fields-of-a-form
+          $("#contactForm").trigger("reset");
+          //End Credit
+          alert("Email Sent!", response.status, response.text);
+        },
+        function (error) {
+          alert("FAILED...", error);
+          console.log("FAILED...", error);
+        }
+      );
+  
+    return false;
+  }
